1c45c2826a16190c68b31dbf6ba8118306be22ce
migusmp <mmp2004sp@gmail.com>
2026-03-26 23:06:21 +0000

    go lint update on nvim

--- GentlemanNvim/nvim/lua/config/autocmds.lua
+++ GentlemanNvim/nvim/lua/config/autocmds.lua
@@ -1,3 +1,12 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- nvim-treesitter doesn't auto-enable highlight for Go in Neovim 0.11.
+-- Force-start the native treesitter highlighter on every Go buffer.
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "go",
+  callback = function()
+    pcall(vim.treesitter.start)
+  end,
+})
